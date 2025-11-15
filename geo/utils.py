252f8533046c030d4fc9acb56d6122d765b5bcc6
soyoung0108 <ksy030108@naver.com>
2025-11-15 16:08:29 +0900

    final attempt

--- geo/utils.py
+++ geo/utils.py
@@ -1,11 +1,9 @@
 import math
 
-# 피타고라스 정리: 빗변 c의 길이 계산 (c = sqrt(a**2 + b**2))
 def pythagoras(a, b):
     c = math.sqrt(a**2 + b**2)
     return c
 
-# 원의 넓이 계산 (area = pi * r * 2)
 def circle(r):
     area = math.pi * r * 2
     return area
--- tester.py
+++ tester.py
@@ -1,11 +1,9 @@
 import geo.utils as utils 
 
-# 피타고라스 정리 (a=3, b=4)
 a, b = 3, 4
 c = utils.pythagoras(a, b)
 print('c =', c)
 
-# 원의 넓이 (r=10)
 r = 10
 area = utils.circle(r)
 print('area =', area)
